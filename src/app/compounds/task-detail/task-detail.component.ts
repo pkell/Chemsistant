cafa758716da8cf8137b74933fb447b2b52c9b02
Paul Kelly <>
2019-10-21 18:34:20 +0100

    css changes and task detail component

--- src/app/compounds/task-detail/task-detail.component.ts
+++ src/app/compounds/task-detail/task-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { FirebaseService } from '../services/firebase.service';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-task-detail',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-detail.component.scss']
 })
 export class TaskDetailComponent implements OnInit {
-
-  constructor() { }
+  id: string
+  task: Task;
+  constructor(
+    private route: ActivatedRoute, 
+    private dialog: MatDialog, 
+    public firebaseService: FirebaseService
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id');
+      this.getTask();
+    });
+  }
+
+  getTask() {
+    this.firebaseService.getTask(this.id).subscribe(data => {
+      this.task = data;
+    });
   }
 
 }
